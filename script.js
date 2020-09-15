918dce9044c85b64dc885b83dc02e5f20bb3660e
Jakob Nordhagen <jakobn@stanford.edu>
2020-09-14 17:52:42 -0700

    Bug fix randint

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Jakob Nordhagen
 // This file implements the functionality of the number guessing game.
 
-let randomNumber = 37;
+let randomNumber = Math.floor(Math.random() * 100) + 1;
 
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
